f109cb1039313781102e77049795298f86a350b4
sozgat <said.ozgat@hotmail.com>
2021-12-19 03:18:57 +0300

    +Swagger and property file edit.

--- src/main/java/com/eteration/simplebanking/config/SwaggerConfig.java
+++ src/main/java/com/eteration/simplebanking/config/SwaggerConfig.java
@@ -2,17 +2,12 @@ package com.eteration.simplebanking.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.google.common.base.Predicate;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
-import static com.google.common.base.Predicates.or;
 
 @Configuration
 @EnableSwagger2
